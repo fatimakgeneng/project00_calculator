b0ef06c924fda4de5c37783f27d2998cb1862e71
Fatima Khan <you@example.com>
2024-03-20 23:17:56 +0500

    Made some minor changes in the user prompt and added a console.log

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstnumber" },
-  { message: "Enter second number", type: "number", name: "secondnumber" },
+  { message: "Enter your first number", type: "number", name: "firstnumber" },
+  { message: "Enter your second number", type: "number", name: "secondnumber" },
   {
     message: "Select one of the operators to perform operation",
     type: "list",
@@ -28,3 +28,5 @@ else if(answer.operator === "Division") {
 } else {
     console.log("Please select valid operator")
 }
+
+console.log("Thank you for using");
